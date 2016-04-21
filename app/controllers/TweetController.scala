03cbe4ba3fb0cd65ccddcbfa42a643410e75fc82
kkrzys <klimekkrzys@gmail.com>
2016-04-22 00:20:00 +0200

    added counting number of tweets and number of users in mongodb

--- app/controllers/TweetController.scala
+++ app/controllers/TweetController.scala
@@ -7,22 +7,27 @@ import play.api.mvc._
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 import play.modules.reactivemongo.json._
 import reactivemongo.api.Cursor
+import reactivemongo.api.collections.bson.BSONCollection
+import reactivemongo.bson.BSONDocument
 import reactivemongo.play.json.collection.JSONCollection
+import reactivemongo.api.commands.bson.BSONCountCommand.{Count, CountResult}
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import reactivemongo.api.commands.bson.BSONCountCommandImplicits._
 
 @Singleton
 class TweetController @Inject() (val reactiveMongoApi: ReactiveMongoApi)
   extends Controller with MongoController with ReactiveMongoComponents {
 
-  def collection: JSONCollection = reactiveMongoApi.db.collection[JSONCollection]("tweets")
+  def jsonCollection: JSONCollection = reactiveMongoApi.db.collection[JSONCollection]("tweets")
+  def bsonCollection: BSONCollection = reactiveMongoApi.db.collection[BSONCollection]("tweets")
 
   import models._
   import models.TweetFormats._
 
   def findAll = Action.async {
-    val cursor: Cursor[Tweet] = collection.find(Json.obj()).cursor[Tweet]()
+    val cursor: Cursor[Tweet] = jsonCollection.find(Json.obj()).cursor[Tweet]()
 
     val futureTweetsList: Future[List[Tweet]] = cursor.collect[List]()
 
@@ -31,6 +36,15 @@ class TweetController @Inject() (val reactiveMongoApi: ReactiveMongoApi)
     }
     futureTweetsJsonArray.map { tweets =>
       Ok(tweets)
+    }
+  }
+
+  def count = Action.async {
+    val command = Count(BSONDocument.empty)
+    val resultF: Future[CountResult] = bsonCollection.runCommand(command)
+    resultF.map { res =>
+      val numOfDocuments: Int = res.value
+      Ok(Json.obj("numOfTweets" -> numOfDocuments))
     }
   }
 
--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -7,22 +7,27 @@ import play.api.mvc._
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 import play.modules.reactivemongo.json._
 import reactivemongo.api.Cursor
+import reactivemongo.api.collections.bson.BSONCollection
+import reactivemongo.bson.BSONDocument
 import reactivemongo.play.json.collection.JSONCollection
+import reactivemongo.api.commands.bson.BSONCountCommand.{Count, CountResult}
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import reactivemongo.api.commands.bson.BSONCountCommandImplicits._
 
 @Singleton
 class UserController @Inject() (val reactiveMongoApi: ReactiveMongoApi)
   extends Controller with MongoController with ReactiveMongoComponents {
 
-  def collection: JSONCollection = reactiveMongoApi.db.collection[JSONCollection]("users")
+  def jsonCollection: JSONCollection = reactiveMongoApi.db.collection[JSONCollection]("users")
+  def bsonCollection: BSONCollection = reactiveMongoApi.db.collection[BSONCollection]("users")
 
   import models._
   import models.UserFormats._
 
   def findAll = Action.async {
-    val cursor: Cursor[User] = collection.find(Json.obj()).cursor[User]()
+    val cursor: Cursor[User] = jsonCollection.find(Json.obj()).cursor[User]()
 
     val futureUsersList: Future[List[User]] = cursor.collect[List]()
 
@@ -31,6 +36,15 @@ class UserController @Inject() (val reactiveMongoApi: ReactiveMongoApi)
     }
     futureUsersJsonArray.map { users =>
       Ok(users)
+    }
+  }
+
+  def count = Action.async {
+    val command = Count(BSONDocument.empty)
+    val resultF: Future[CountResult] = bsonCollection.runCommand(command)
+    resultF.map { res =>
+      val numOfDocuments: Int = res.value
+      Ok(Json.obj("numOfUsers" -> numOfDocuments))
     }
   }
 
